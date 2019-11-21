df386312d97c3ce4cd24909ca3e2f180abcbf4eb
freddiejbawden <freddiejbawden@gmail.com>
2019-11-21 15:25:30 +0000

    Clean Warnings

--- app/src/main/java/com/specknet/orientandroid/MainActivity.java
+++ app/src/main/java/com/specknet/orientandroid/MainActivity.java
@@ -4,26 +4,11 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.Debug;
-import android.os.Environment;
-import android.text.Layout;
 import android.util.Log;
-import android.util.Pair;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,34 +16,22 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.series.PointsGraphSeries;
-import com.opencsv.CSVWriter;
 import com.polidea.rxandroidble2.RxBleClient;
-import com.polidea.rxandroidble2.RxBleConnection;
 import com.polidea.rxandroidble2.RxBleDevice;
 import com.polidea.rxandroidble2.scan.ScanSettings;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Observable;
 import java.util.UUID;
 
-import io.reactivex.Single;
+import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
-import static java.lang.Integer.toUnsignedLong;
 
 public class MainActivity extends Activity {
 
@@ -69,23 +42,13 @@ public class MainActivity extends Activity {
 
     private static final String MAG_CHARACTERISTIC = "0000a001-0000-1000-8000-00805f9b34fb";
 
-    private static final boolean raw = true;
-    private RxBleDevice orient_device;
     private Disposable scanSubscription;
     private RxBleClient rxBleClient;
-    private ByteBuffer packetData;
-
-    //private int n = 0;
 
     boolean connected = false;
-    private int counter = 0;
 
-
-    private Button step_on_off_button;
-    private Button reset;
     private Context ctx;
     private ImageView back;
-
 
     private TextView stepView;
     private LineGraphSeries<DataPoint> seriesX;
@@ -93,45 +56,27 @@ public class MainActivity extends Activity {
     private LineGraphSeries<DataPoint> seriesZ;
 
     private int totalSteps;
-    private GraphView graph;
     private int inputCounter = 0;
-    private int inputCounter2 = 0;
     private final int RC_LOCATION_AND_STORAGE = 1;
-    private  ArrayList<Double> mags = new ArrayList<Double>();
+    private  ArrayList<Double> mags = new ArrayList<>();
 
     private StepCounter scPeak = new StepCounter(2.75,0.33333,20,10);
-    private StepCounter scValley = new StepCounter(2.75,0.2,20,10);
+    private StepCounter scValley = new StepCounter(2.75,0.33333,20,10);
 
     private boolean stepSwitch = false;
-    private PointsGraphSeries<DataPoint> peakDatapoints;
     private long init_time = 0;
-    private long time_passed = 0;
-    private int stepsInSequance;
-    private SensorManager sensorManager;
-    private Sensor accSensor;
-    private Sensor gyroSensor;
     private ArrayList<Double> gyroSmooth = new ArrayList<>();
-    private ArrayList<Double> magSmooth = new ArrayList<>();
     private ArrayList<Double> magStdList = new ArrayList<>();
 
     private int rising_edge_step_cache = 0;
     private int temp_stepCount;
     private int walkingFlag;
-    private long timeSinceSwitch = 0L;
-    private  long switchThresh = 1000L;
-    private long switchStartTime = 0L;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        //sensorManager.registerListener(this,gyroSensor,SensorManager.SENSOR_DELAY_UI);
-
-        //sensorManager.registerListener(this,accSensor,SensorManager.SENSOR_DELAY_UI);
         ctx = this;
-        graph = (GraphView) findViewById(R.id.graph);
+        GraphView graph = findViewById(R.id.graph);
         Viewport vp = graph.getViewport();
         vp.setXAxisBoundsManual(true);
         vp.setMinX(0);
@@ -148,25 +93,11 @@ public class MainActivity extends Activity {
         graph.addSeries(seriesY);
         graph.addSeries(seriesZ);
 
-        reset = findViewById(R.id.reset);
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                totalSteps = 0;
-                //scPeak.resetSteps();
-                //scValley.resetSteps();
-            }
-        });
+        Button reset = findViewById(R.id.reset);
+
+        reset.setOnClickListener((View view) -> totalSteps = 0);
 
         getPermissions();
-    }
-
-    public double calcMagFromVals(float[] values){
-        double sum = 0.0;
-        for (float v : values){
-            sum += Math.pow((((double) v)),2.0);
-        }
-        return Math.sqrt(sum)-9.8;
     }
 
     @AfterPermissionGranted(RC_LOCATION_AND_STORAGE)
@@ -185,7 +116,7 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         // Forward results to EasyPermissions
@@ -195,19 +126,11 @@ public class MainActivity extends Activity {
     private void runApp() {
         stepView = findViewById(R.id.steps);
 
-        step_on_off_button = findViewById(R.id.onOff);
-        step_on_off_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                stepSwitch = !stepSwitch;
-            }
-        });
+        Button step_on_off_button = findViewById(R.id.onOff);
+        step_on_off_button.setOnClickListener((View v) -> stepSwitch = !stepSwitch);
         back = findViewById(R.id.movingIndicator);
 
-
-
-
-        packetData = ByteBuffer.allocate(18);
+        ByteBuffer packetData = ByteBuffer.allocate(18);
         packetData.order(ByteOrder.LITTLE_ENDIAN);
 
         rxBleClient = RxBleClient.create(this);
@@ -225,26 +148,26 @@ public class MainActivity extends Activity {
                                     scanResult.getBleDevice().getMacAddress());
                             // Process scan result here.
                             if (scanResult.getBleDevice().getMacAddress().equals(ORIENT_BLE_ADDRESS)) {
-                                runOnUiThread(() -> {
+                                runOnUiThread(() ->
                                     Toast.makeText(ctx, "Found " + scanResult.getBleDevice().getName() + ", " +
                                                     scanResult.getBleDevice().getMacAddress(),
-                                            Toast.LENGTH_SHORT).show();
-                                });
+                                            Toast.LENGTH_SHORT).show()
+                                );
                                 connectToOrient(ORIENT_BLE_ADDRESS);
                                 scanSubscription.dispose();
                             }
                         },
                         throwable -> {
                             // Handle an error here.
-                            runOnUiThread(() -> {
+                            runOnUiThread(() ->
                                 Toast.makeText(ctx, "BLE scanning error",
-                                        Toast.LENGTH_SHORT).show();
-                            });
+                                        Toast.LENGTH_SHORT).show()
+                            );
                         }
                 );
     }
     private void connectToOrient(String addr) {
-        orient_device = rxBleClient.getBleDevice(addr);
+        RxBleDevice orient_device = rxBleClient.getBleDevice(addr);
         Log.d("BLE", "Connecting");
         orient_device.establishConnection(true)
                 .flatMap(rxBleConnection -> rxBleConnection.setupNotification(UUID.fromString(MAG_CHARACTERISTIC)))
@@ -299,17 +222,7 @@ public class MainActivity extends Activity {
         }
         */
     }
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
     public static ByteBuffer bytesToInt(final byte[] array, final int start)
     {
         final ByteBuffer buf = ByteBuffer.wrap(array); // big endian by default
@@ -372,7 +285,6 @@ public class MainActivity extends Activity {
             walkingFlag = 0;
             rising_edge_step_cache = 0;
         }
-        counter += 1;
         runOnUiThread(() -> {
             seriesY.appendData(new DataPoint(inputCounter, mag_sd),true,500);
             seriesX.appendData(new DataPoint(inputCounter, averageGyro/3),true,500);
